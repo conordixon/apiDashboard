8fc4b6611f273ddbb3c347a6cb025ebad89eaf56
Conor Dixon <conor.dixon@gmail.com>
2021-02-14 23:29:23 +0000

    test api call

--- src/main/java/TestAPICall.java
+++ src/main/java/TestAPICall.java
@@ -4,18 +4,21 @@ import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.*;
+import org.junit.Test;
+
 
 
 
 public class TestAPICall {
 
+    @Test
     public void TestAPICall(){
         given().
                 given().
                 contentType("text/json").
                 when().
-                get("https://swapi.dev/api/people/1").
+                get("http://www.google.com").
                 then().
-                statusCode(401);
+                statusCode(200);
     }
 }
